4ca9f822d5d96e9058ce7e2327169fa0d6b6e09a
Sergi Virgili <svirgilif@gmail.com>
2023-01-26 19:57:31 +0100

    slower shadow

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { TodoList } from "./components/TodoList";
 
 function App() {
   return (
-    <div className="">
+    <div className="app">
       <NavBar />
       {/* <Hero /> */}
       {/* <DreamCRUD /> */}
