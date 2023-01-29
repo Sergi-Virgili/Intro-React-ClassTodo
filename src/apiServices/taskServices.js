c66f45e5f5e6fba11c9c4f980ca704a65e976a87
Sergi Virgili <svirgilif@gmail.com>
2023-01-29 14:27:43 +0100

    add api get List

--- src/apiServices/taskServices.js
+++ src/apiServices/taskServices.js
@@ -1,13 +1,21 @@
 import { dataBase } from "../data/database";
+import TaskModel from "../models/Task";
 import Task from "../models/Task";
 
+const apiBase = process.env.REACT_APP_API_BASE;
+
 const taskServices = {
+  // getAllData: () => {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve(dataBase.map((x) => new Task(x)));
+  //     }, 3000);
+  //   });
+  // },
   getAllData: () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(dataBase.map((x) => new Task(x)));
-      }, 3000);
-    });
+    return fetch(apiBase)
+      .then((res) => res.json())
+      .then((data) => data.map((task) => new TaskModel(task)));
   },
 };
 
